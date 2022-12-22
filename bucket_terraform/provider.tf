a505ed80163e5900e3cc747eaf377e6837de49e3
roberto-asir <you@example.com>
2022-12-22 17:21:28 +0100

    FIX comento la configuración del provider porque era algo propio del PC de Carlos, así se puede usar por cualquiera configurando el entorno

--- bucket_terraform/provider.tf
+++ bucket_terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "google" {
   # Configuration options
-  credentials = file("/home/carlosfeu/Documentos/cosmic-tensor-371107-46c1046641d8.json")
+  # credentials = file("/home/carlosfeu/Documentos/cosmic-tensor-371107-46c1046641d8.json")
   project = "cosmic-tensor-371107"
   region = "us-central1"
   zone = "us-central1-c"
